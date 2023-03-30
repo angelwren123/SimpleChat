51d07befd62045767a51f60d1e3be8dff000fac7
angelwren123 <rync2301@gmail.com>
2023-03-30 14:26:49 +0700

    update file

--- App.js
+++ App.js
@@ -26,158 +26,38 @@ export default App
 const styles = StyleSheet.create({})
 
 
-
-// // import React, {useEffect} from 'react';
-// // import {View, Button, Alert} from 'react-native';
-// // import notifee from '@notifee/react-native';
-// // import messaging from '@react-native-firebase/messaging';
-// // import NotificationService from './NotificationService';
-
-// // function App() {
-// //   useEffect(() => {
-// //     getFCMToken();
-// //     requestPermission();
-// //     const unsubscribe = messaging().onMessage(async remoteMessage => {
-// //       console.log('remoteMessage', JSON.stringify(remoteMessage));
-// //       DisplayNotification(remoteMessage);
-// //       // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-// //     });
-// //     return unsubscribe;
-// //   }, []);
-
-// //   const getFCMToken = () => {
-// //     messaging()
-// //       .getToken()
-// //       .then(token => {
-// //         console.log('token=>>>', token);
-// //       });
-// //   };
-
-// //   const requestPermission = async () => {
-// //     const authStatus = await messaging().requestPermission();
-// //   };
-
-// //   async function DisplayNotification(remoteMessage) {
-// //     // Create a channel
-// //     const channelId = await notifee.createChannel({
-// //       id: 'default',
-// //       name: 'Default Channel',
-// //     });
-
-// //     // Display a notification
-// //     await notifee.displayNotification({
-// //       title: remoteMessage.notification.title,
-// //       body: remoteMessage.notification.body,
-// //       android: {
-// //         channelId,
-// //         smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
-// //       },
-// //     });
-// //   }
-
-// //   async function localDisplayNotification() {
-// //     // Create a channel
-// //     const channelId = await notifee.createChannel({
-// //       id: 'default',
-// //       name: 'Default Channel',
-// //     });
-
-// //     // Display a notification
-// //     notifee.displayNotification({
-// //       title:
-// //         '<p style="color: #4caf50;"><b>Styled HTMLTitle</span></p></b></p> &#128576;',
-// //       subtitle: '&#129395;',
-// //       body: 'The <p style="text-decoration: line-through">body can</p> also be <p style="color: #ffffff; background-color: #9c27b0"><i>styled too</i></p> &#127881;!',
-// //       android: {
-// //         channelId,
-// //         color: '#4caf50',
-// //         actions: [
-// //           {
-// //             title: '<b>Dance</b> &#128111;',
-// //             pressAction: {id: 'dance'},
-// //           },
-// //           {
-// //             title: '<p style="color: #f44336;"><b>Cry</b> &#128557;</p>',
-// //             pressAction: {id: 'cry'},
-// //           },
-// //         ],
-// //       },
-// //     });
-// //   }
-
-// //   const sendNotification = async () => {
-// //     let notificationData = {
-// //       title: 'First Notification',
-// //       body: 'Notification Body',
-// //       token:
-// //         'cCBdYjsgRZGyUj8V3oqzQp:APA91bH3NQoAekhLzw4IBCy_vwYbnJMUZaYC46cSUr4gdqCLo2EbQVy94gtyY0yZNClZSZjAlN1TxkrlPIJQXk9hgKm5k6TMv6JPH5u8P0jrJf_cQ2i_eZbf1hpgjmFg4TgibXw-nRnr',
-// //     };
-// //     await NotificationService.sendSingleDeviceNotification(notificationData);
-// //   };
-
-// //   const sendMultiNotification = async () => {
-// //     let notificationData = {
-// //       title: 'First Multi Device Notification',
-// //       body: 'Notification Body',
-// //       token: [
-// //         'cCBdYjsgRZGyUj8V3oqzQp:APA91bH3NQoAekhLzw4IBCy_vwYbnJMUZaYC46cSUr4gdqCLo2EbQVy94gtyY0yZNClZSZjAlN1TxkrlPIJQXk9hgKm5k6TMv6JPH5u8P0jrJf_cQ2i_eZbf1hpgjmFg4TgibXw-nRnr',
-// //       ],
-// //     };
-// //     await NotificationService.sendMultiDeviceNotification(notificationData);
-// //   };
-
-// //   return (
-// //     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-// //       <Button title="Send Notification" onPress={sendNotification} />
-// //       <Button
-// //         title="Send Multi Device Notification"
-// //         onPress={sendMultiNotification}
-// //       />
-// //     </View>
-// //   );
-// // }
-
-// // export default App;
-
-
-
-// import React from "react";
-// import { Text, TouchableOpacity } from "react-native";
-// import Spinner from 'react-native-loading-spinner-overlay';
-// function useChange() {
-//   const [state, setState] = React.useState(0);
-//   function change(value) {
-//     setState(value);
+// import React, { Component } from 'react';
+// import { View } from 'react-native';
+// import { LoginButton, AccessToken, Settings } from 'react-native-fbsdk-next';
+// import SplashScreen from 'react-native-splash-screen';
+// export default class Login extends Component {
+//   componentDidMount() {
+//     // do stuff while splash screen is shown
+//       // After having done stuff (such as async tasks) hide the splash screen
+//       Settings.setAppID('746489427139440');
+//       SplashScreen.hide();
 //   }
-
-//   return { change, state };
-// }
-// export const useChangeLoader = () => {
-//   const [state, setState] = React.useState(false);
-//   function showLoader() {
-//     setState(true);
+//   render() {
+//     return (
+//       <View>
+//         <LoginButton
+//           onLoginFinished={
+//             (error, result) => {
+//               if (error) {
+//                 console.log("login has error: " + result.error);
+//               } else if (result.isCancelled) {
+//                 console.log("login is cancelled.");
+//               } else {
+//                 AccessToken.getCurrentAccessToken().then(
+//                   (data) => {
+//                     console.log(data.accessToken.toString())
+//                   }
+//                 )
+//               }
+//             }
+//           }
+//           onLogoutFinished={() => console.log("logout.")}/>
+//       </View>
+//     );
 //   }
-//   function hideLoader() {
-//     setState(false);
-//   }
-//   return [state, showLoader, hideLoader];
-// }
-
-
-// export default App = () => {
-//   const [state,showLoader] = useChangeLoader();
-//   return (
-//     <>
-//       <TouchableOpacity
-//         onPress={() => {
-//           // Update state value on press
-//           showLoader()
-//         }}
-//       >
-//         <Text>Click Me!{state}</Text>
-//       </TouchableOpacity>
-//       <Spinner visible={state} />
-
-//     </>
-//   );
-// }
+// };
